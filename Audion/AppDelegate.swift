0ee79258c57f3091c10ecc3a342910186012b9ca
Michael Buckley <michael@panic.com>
2020-05-14 06:27:25 -0700

    Initial Commit

---
+++ Audion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Audion
+//
+//  Created by Buckley on 5/14/20.
+//  Copyright © 2020 Panic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Audion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Audion
+//
+//  Created by Buckley on 5/14/20.
+//  Copyright © 2020 Panic. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
