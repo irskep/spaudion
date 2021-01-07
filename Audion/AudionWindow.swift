e8ad6f01a07ff2cd5370bb7a87f11ecd28f71988
Michael Buckley <michael@panic.com>
2021-01-07 15:31:58 -0800

    Add GPLv3 License

--- Audion/AudionWindow.swift
+++ Audion/AudionWindow.swift
@@ -1,10 +1,21 @@
-//
-//  AudionWindow.swift
-//  Audion
-//
-//  Created by Buckley on 5/14/20.
-//  Copyright © 2020 Panic. All rights reserved.
-//
+/*
+Copyright 2020-2021 Panic Inc.
+
+This file is part of Audion.
+
+Audion is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Audion is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Audion.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 import Cocoa
 
--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This repository contains the source code to a lite version of Audion that can vi
 
 The latest build can be doanloaded from https://download.panic.com/audion-viewer/Audion.app.zip
 
+## License
+
+This code is licensed under the GPLv3, in order to prevent it from being used on the App Store.
+
+If you wish to license it for commercial purposes, get in touch.
+
 ## Intalling Faces
 
 This version of Audion only supports faces that had been converted to a modern format. You can download the faces from https://download.panic.com/audion-viewer. Once downloaded, unzip them, launch Audion, select `Open Faces Folder` from the `File` menu. A new empty Finder window will open. Select all the face folders and drag them into this empty window.
